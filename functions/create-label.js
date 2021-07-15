0a01e17b059c43504417a0af11050d93a174f7cc
Wonmin Lee <won2blee@gmail.com>
2021-07-15 18:23:23 -0400

    tracking info sent to customer

--- functions/create-label.js
+++ functions/create-label.js
@@ -2,6 +2,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Easypost = require('@easypost/api');
 const api = new Easypost(process.env.EASYPOST_API_KEY);
 
+//<editor-fold>------------------------------------------SETUP---------------------------------------
+
 //sendgrid email stuff
 const client = require('@sendgrid/mail');
 const {
@@ -21,10 +23,36 @@ const fromAddress = new api.Address({
 });
 fromAddress.save();
 
+//</editor-fold>
+
+//<editor-fold>------------------------------------------FUNCTIONS---------------------------------------
+
 //catcher for errors
 const errorCatch = (error) => {
   console.log(error);
 }
+
+//send an email to messageDetails.toEmail
+const sendEmail = async (messageDetails) => {
+  const data = {
+    to: messageDetails.toEmail,
+    from: {
+      email: SENDGRID_FROM_EMAIL,
+      name: "Pegasus Games",
+    },
+    subject: messageDetails.subject,
+    html: messageDetails.message,
+  };
+
+  try {
+    await client.send(data);
+    console.log("Successfully sent an email!");
+  } catch (err) {
+    errorCatch(err);
+  }
+}
+
+//</editor-fold>
 
 //create the label via easypost after receiving successful stripe session_id
 const createLabel = async (session_id) => {
@@ -38,7 +66,8 @@ const createLabel = async (session_id) => {
     city: session.shipping.address.city,
     state: session.shipping.address.state,
     zip: session.shipping.address.postal_code,
-    country: session.shipping.address.country
+    country: session.shipping.address.country,
+    email: session.customer_email
   });
   toAddress.save().catch(errorCatch);
 
@@ -60,6 +89,8 @@ const createLabel = async (session_id) => {
   shipment.save().then(s =>
     s.buy(shipment.lowestRate(), 40.00)
      .then(s => {
+
+       //send label to myself
        sendEmail({
          toEmail: "pegasusnotunicorn+sendgrid@gmail.com",
          subject: `$${s.selected_rate.rate} shipping label was purchased for Sysifus Corp! SHIPSYSIFUSLABEL - ${s.to_address.name}`,
@@ -74,27 +105,31 @@ const createLabel = async (session_id) => {
                     <p>Label Cost - $${s.selected_rate.rate} ${s.selected_rate.currency}<p>
                   `
        });
-     })
-  ).catch(errorCatch);
-}
 
-//send an email to messageDetails.toEmail
-const sendEmail = async (messageDetails) => {
-  const data = {
-    to: messageDetails.toEmail,
-    from: SENDGRID_FROM_EMAIL,
-    name: "Pegasus Games",
-    fromname: "Pegasus Games",
-    subject: messageDetails.subject,
-    html: messageDetails.message,
-  };
+       //send tracking info to customer
+       sendEmail({
+         toEmail: session.customer_details.email,
+         subject: `Welcome to Sysifus Corp, ${s.to_address.name}! Here is your shipping info.`,
+         message: `
+                    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                    <html>
+                      <head>
+                        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+                      </head>
+                      <body style='text-align:center; margin:auto;'>
+                        <h1>Welcome to Sysifus Corp!</h1>
+                        <h4>A cut-throat corporate board game</h4>
+                        <img style="width:500px;" src="https://www.sysifuscorp.com/assets/images/gallery/1-min.jpg" />
+                        <p>Your copy of the game is being shipped to ${s.to_address.city}</p>
+                        <p>Here is the tracking # - <a href="${s.tracker.tracking_details.public_url}">${s.tracking_code}</a><p>
+                        <p>If there are any issues with the shipment or if you have any questions at all, please do not hesitate to email us at hello@sysifuscorp.com</p>
+                      </body>
+                    </html>
+                  `
+       });
 
-  try {
-    await client.send(data);
-    console.log("Successfully sent an email!");
-  } catch (err) {
-    errorCatch(err);
-  }
+     }))
+     .catch(errorCatch);
 }
 
 exports.handler = async (event, context, callback) => {
