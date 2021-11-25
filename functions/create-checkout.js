64cb0f2b02e3734709b903ff6fee27971002e23e
Wonmin Lee <won2blee@gmail.com>
2021-11-25 14:17:47 -0500

    adding several new european countries

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     payment_method_types: ['card'],
     billing_address_collection: 'auto',
     shipping_address_collection: {
-      allowed_countries: ['US', 'CA', "MX", "AT", "BE", "DK", "FR", "FI", "DE", "GR", "HU", "IE", "JP", "KR", "NL", "NO", "PT", "SG", "SE", "ES", "GB"],    
+      allowed_countries: ['US', 'CA', "MX", "CH", "AT", "BE", "DK", "FR", "FI", "DE", "GR", "HU", "IE", "IS", "IL", "IT", "LU", "JP", "KR", "NL", "NO", "PT", "SG", "SE", "ES", "GB"],
     },
     mode: "payment",
     // success_url: `${process.env.URL}/.netlify/functions/create-label?session_id={CHECKOUT_SESSION_ID}`,
